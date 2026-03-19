c2671b8bed54fe09be6f6fe69acb7d56b5bf9176
Leonardo Reuter <lreuter@imply.com>
2026-03-18 21:26:06 -0300

    final - separando trabalhos

--- bfs-astar-maze/maze.py
+++ bfs-astar-maze/maze.py
@@ -1,7 +1,7 @@
 from pyamaze import maze, agent
 import astar, bfs
 
-SIZE=50
+SIZE=25
 
 explore = True if str(input("Acompanhar visualmente a exploração? (y,N)  ")).upper() == "Y" else False
 loop    = True if str(input("Mapa tem loop? (y,N)  ")).upper() == "Y" else False
@@ -21,10 +21,12 @@ print("\n\n\tRESULTADOS")
 print("--------------------")
 print(f"MAZE size: {m.cols}x{m.rows}")
 print("--------------------")
+print(f"BFS path:       {[p for p  in path_bfs]}")
 print(f"BFS lenght:     {len(path_bfs)}")
 print(f"BFS nodes:      {total_nodes_bfs}")
 print(f"BFS time:       {time_bfs:.6f}")
 print("--------------------")
+print(f"A-STAR path:    {[p for p in path_astar]}")
 print(f"A-STAR lenght:  {len(path_astar)}")
 print(f"A-STAR nodes:   {total_nodes_astar}")
 print(f"A-STAR time:    {time_astar:.6f}")
--- mazes-generate.py
+++
@@ -1,9 +0,0 @@
-from pyamaze import maze, agent
-
-m500 = maze(500,500)
-m1000 = maze(1000,1000)
-
-m500.CreateMaze(saveMaze=True)
-m500.CreateMaze(loopPercent=100, saveMaze=True)
-m1000.CreateMaze(loopPercent=100, saveMaze=True)
-m1000.CreateMaze(saveMaze=True)
